559dd464d493093496978ceafabaa2a326d4755b
BadTripHD <baptistepollet7@gmail.com>
2021-03-15 16:08:53 +0100

    First commit

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
 const { static } = require("express")
 const express = require("express")
 const app = express()
+const fs = require('fs');
 const axios = require("axios");
 const API_BASE_URL = "https://api.punkapi.com/v2/"
 
 app.set("view engine", "ejs")
 app.use("/css", express.static(__dirname + "/css"))
 
+axios.get(API_BASE_URL + "beers")
+    .then(response =>{
+        let beers = JSON.stringify(response.data, null, 2)
+        fs.writeFileSync("beers.json", beers, (error) => {
+            if (error) throw error
+            console.log("Data write in json file")
+        })
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
 //----------------MAIN PAGE---------------------->
 
 app.get("/", (req, res) => {
-
-    axios.get(API_BASE_URL + "beers")
-        .then(response =>{
-            let beers = response.data
-            res.render("index", {
-                beers: beers
-            })
-        })
-        .catch(error => {
-            console.log(error)
-        })
+    let rawdata = fs.readFileSync("beers.json")
+    let beers = JSON.parse(rawdata)
+    res.render("index", {
+        beers: beers
+    })
 
 })
 
